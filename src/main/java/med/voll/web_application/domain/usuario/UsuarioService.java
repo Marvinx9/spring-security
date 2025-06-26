c66044bdf0d2d31caea2c818984e43466efa895a
Afrânio Nunes Dantas <afraniodantas0224@gmail.com>
2025-06-25 23:24:53 -0300

    feat: add usuario and medico

--- src/main/java/med/voll/web_application/domain/usuario/UsuarioService.java
+++ src/main/java/med/voll/web_application/domain/usuario/UsuarioService.java
@@ -1,20 +1,32 @@
 package med.voll.web_application.domain.usuario;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsuarioService implements UserDetailsService {
     private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder encriptador;
 
-    public UsuarioService(UsuarioRepository usuarioRepository) {
+    public UsuarioService(UsuarioRepository usuarioRepository, PasswordEncoder encriptador) {
         this.usuarioRepository = usuarioRepository;
+        this.encriptador = encriptador;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return usuarioRepository.findByEmailIgnoreCase(username).orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado!"));
+    }
+
+    public void salvarUsuario(String nome, String email, String senha) {
+        String senhaCriptografada = encriptador.encode(senha);
+
+        usuarioRepository.save(new Usuario(nome, email, senhaCriptografada));
     }
 }
